2d9e71152727783d74e07455d579afb32603b232
vikash kumar <47270358+kvikash668@users.noreply.github.com>
2022-02-13 12:08:11 +0530

    stored_procedure

---
+++ Stored_procedure.sql
@@ -0,0 +1,7 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `stored_procedure`()
+select supplier.supp_id,supplier.supp_name,rating.rat_ratstars,
+CASE
+    WHEN rating.rat_ratstars >4 THEN 'Genuine Supplier'
+    WHEN rating.rat_ratstars>2 THEN 'Average Supplier'
+    ELSE 'Supplier should not be considered'
+END AS verdict from rating inner join supplier on supplier.supp_id=rating.supp_id
